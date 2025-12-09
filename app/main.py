e1176f9d6a4192831c284ee08032a7e0c80155a9
Nazyarts1 <nazyarts123@gmail.com>
2025-12-09 19:59:12 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
 
+from fastapi import FastAPI
+from app.video import generate_video
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "AI Video App is running"}
+
+@app.post("/generate")
+def create_video(prompt: str):
+    result = generate_video(prompt)
+    return {"status": "success", "video_url": result}
